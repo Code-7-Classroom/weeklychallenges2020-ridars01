d2346ffca6ee750156476199a08ba39ce236eec0
Rida Fatimah <ridafatimah@MacBook-Pro-3.local>
2020-03-30 11:44:06 -0400

    Added Array Chekpoint

---
+++ checkpoint/chekPointArrays.js
@@ -0,0 +1,23 @@
+//Name 3 Java Script Array Methods
+        // .push 
+        //.pop
+        //.indexOf
+
+
+//Code 
+
+var drinks = [] ; 
+drinks.push("coffee") ; 
+drinks.push("tea") ; 
+drinks.push("water") ; 
+console.log(drinks) ; 
+var foods = [] ; 
+foods.push("Apple") ; 
+foods.push("Sandwhich") ; 
+foods.push("Chips") ; 
+console.log(foods) ; 
+var meals = drinks + foods; 
+console.log(meals) ; 
+var mealsReverse = foods + drinks ; 
+console.log(mealsReverse) ; 
+
